b9444df238c3a2ef9c0a162b070e13b677215df0
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-07-02 17:22:21 +0200

    Implemented allBooks()

--- com.giulia.giamberini.book.orders/src/main/java/com/giulia/giamberini/book/orders/controller/BookController.java
+++ com.giulia.giamberini.book.orders/src/main/java/com/giulia/giamberini/book/orders/controller/BookController.java
@@ -1,9 +1,20 @@
 package com.giulia.giamberini.book.orders.controller;
 
+import com.giulia.giamberini.book.orders.repository.BookRepository;
+import com.giulia.giamberini.book.orders.view.BookOrdersView;
+
 public class BookController {
 
+	private BookOrdersView bookOrdersView;
+	private BookRepository bookRepository;
+
+	public BookController(BookOrdersView bookOrdersView, BookRepository bookRepository) {
+		this.bookOrdersView = bookOrdersView;
+		this.bookRepository = bookRepository;
+	}
+
 	public void allBooks() {
-		
+		bookOrdersView.showAllBooks(bookRepository.findAll());
 	}
 
 }
